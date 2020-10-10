cbc23ffb764c7fd2ab7eb7239d1b23253fa478ad
romanza705 <r.mukhtarov@innopolis.university>
2020-10-10 16:25:18 +0300

    Created new app(module). Path to app edited. Default main page changed.

--- InnoClubs/InnoClubs/settings.py
+++ InnoClubs/InnoClubs/settings.py
@@ -10,11 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os, sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+PROJECT_ROOT = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
