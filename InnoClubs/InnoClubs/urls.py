155de4343c7c883425ce7abe43b0afbade974a38
romanza705 <r.mukhtarov@innopolis.university>
2020-10-10 17:27:00 +0300

    Authorization app renamed to authorization. base infrastructure created (static for media, templates for html). link to first (login.)html created

--- InnoClubs/InnoClubs/urls.py
+++ InnoClubs/InnoClubs/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('Authorization.urls')),
+    path('', include('authorization.urls')),
     path('admin/', admin.site.urls),
 ]
