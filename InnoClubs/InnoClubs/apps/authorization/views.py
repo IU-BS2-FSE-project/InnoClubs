aacfd749ebd58e42bdd7c06c5f65aef9e18df7a2
romanza705 <r.mukhtarov@innopolis.university>
2020-10-13 21:34:36 +0300

    All frontend of authorization app added, the backend remain. The transition between pages worked.

--- InnoClubs/InnoClubs/apps/authorization/views.py
+++ InnoClubs/InnoClubs/apps/authorization/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'authorization/login.html')
+
+
+def signup(request):
+    return render(request, 'authorization/registr.html')
