f89147a34ef9c552658df8b7ba249d7d71eeca5b
romanza705 <r.mukhtarov@innopolis.university>
2020-10-14 20:47:08 +0300

    The backend for login form added (as minimum it can say if user data is incorrect) Superuser for me(Roman) created

--- InnoClubs/InnoClubs/apps/authorization/urls.py
+++ InnoClubs/InnoClubs/apps/authorization/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('signup/', views.signup),
+    path('', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
 ]
--- InnoClubs/InnoClubs/apps/authorization/views.py
+++ InnoClubs/InnoClubs/apps/authorization/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_protect
+from django.contrib import auth
 
 
-def index(request):
-    return render(request, 'authorization/login.html')
+@csrf_protect
+def login(request):
+    args = {}
+    if request.POST:
+        username = request.POST.get('uname', '')
+        password = request.POST.get('psw', '')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            args['error'] = "User not found"
+            return render(request, 'authorization/login.html', args)
+    else:
+        return render(request, 'authorization/login.html')
 
 
 def signup(request):
