74c09087ae6cbe181ad9dad2aeb912928e2d26ca
romanza705 <r.mukhtarov@innopolis.university>
2020-10-16 19:26:02 +0300

    some cosmetical changes... also I deleted the model Person, because I used build-in Django auth system with Django's user

--- InnoClubs/InnoClubs/apps/authorization/models.py
+++ InnoClubs/InnoClubs/apps/authorization/models.py
@@ -1,12 +1,1 @@
 from django.db import models
-from django.contrib.auth.models import User
-
-
-class Person(models.Model):    # I want to change this name. Not User. Wait for your ideas
-    login = models.CharField('login', max_length=64)
-    name = models.CharField('name', max_length=32)
-    sname = models.CharField('surname', max_length=32)
-    mail = models.EmailField('e.mail', max_length=64)
-    password = models.CharField('password', max_length=64)  # TODO somehow increase security
-    hours = models.DecimalField('hours/semestr', max_digits=5,
-                                decimal_places=2)  # maybe it is too early for this field
