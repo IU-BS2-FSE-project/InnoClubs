ae25316f4b243153a0b7f952fb5bc35b369cd9d1
romanza705 <r.mukhtarov@innopolis.university>
2020-10-23 17:13:22 +0300

    Comments added, cosmetic improvements

--- InnoClubs/InnoClubs/apps/authorization/forms.py
+++ InnoClubs/InnoClubs/apps/authorization/forms.py
@@ -5,29 +5,29 @@ from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=32,
-                           widget=forms.TextInput(attrs={'placeholder': 'Enter Name'}),
-                           label='',
-                           )
+                                 widget=forms.TextInput(attrs={'placeholder': 'Enter Name'}),
+                                 label='',
+                                 )
 
     last_name = forms.CharField(max_length=32,
-                            widget=forms.TextInput(attrs={'placeholder': 'Enter Surname'}),
-                            label='')
+                                widget=forms.TextInput(attrs={'placeholder': 'Enter Surname'}),
+                                label='')
 
     username = forms.CharField(max_length=64,
                                widget=forms.TextInput(attrs={'placeholder': 'Enter Username'}),
                                label='')
 
     email = forms.EmailField(max_length=64,
-                            widget=forms.EmailInput(attrs={'placeholder': 'Enter Mail'}),
-                            label=''
-                            )
+                             widget=forms.EmailInput(attrs={'placeholder': 'Enter Mail'}),
+                             label=''
+                             )
 
     password1 = forms.CharField(max_length=64,
-                               strip=False,
-                               widget=forms.PasswordInput(attrs={'autocomplete': 'new-password',
-                                                                 'placeholder': 'Enter Password'}),
+                                strip=False,
+                                widget=forms.PasswordInput(attrs={'autocomplete': 'new-password',
+                                                                  'placeholder': 'Enter Password'}),
                                 label=''
-                               )
+                                )
 
     password2 = forms.CharField(max_length=64,
                                 widget=forms.PasswordInput(attrs={'autocomplete': 'new-password',
