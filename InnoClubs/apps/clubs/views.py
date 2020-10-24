abb31b5c7ef46ee6fd8bf82e84329df987ef8a68
Daniil <d.livitin@innopolis.university>
2020-10-24 19:36:19 +0300

    try/catch for the getting info from db

--- InnoClubs/apps/clubs/views.py
+++ InnoClubs/apps/clubs/views.py
@@ -7,7 +7,10 @@ def main(request):
     args = {}
     if auth.get_user(request).username:
         args['username'] = auth.get_user(request).username
-        args['infoFromDb'] = Club.objects.all()
+        try:
+            args['infoFromDb'] = Club.objects.all()
+        except:
+            args['infoFromDb'] = None
         return render(request, "clubs/main.html", args)
     else:
         return redirect("auth/")
@@ -17,7 +20,10 @@ def ClubPage(request, club_url):
     args = {}
     if auth.get_user(request).username:
         args['username'] = auth.get_user(request).username
-        args['infoFromDb'] = Club.objects.get(club_url=club_url)
+        try:
+            args['infoFromDb'] = Club.objects.get(club_url=club_url)
+        except:
+            args['infoFromDb'] = None
         return render(request, "clubs/pageOfClub.html", args)
     else:
         return redirect('auth/')
