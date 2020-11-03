e2ec822959db15b1f9e4faf484e577f3a281b395
Dablup <d.livitin@innopolis.university>
2020-11-03 19:17:00 +0300

    added model clubs in the settings

--- InnoClubs/InnoClubs/settings.py
+++ InnoClubs/InnoClubs/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'clubs.apps.ClubsConfig',
     'authorization.apps.AuthorizationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
