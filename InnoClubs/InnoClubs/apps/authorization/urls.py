c8b8064aec225f4cea52f98cf8e8064f20390745
Dablup <44948387+Dablup@users.noreply.github.com>
2020-11-04 01:19:12 +0500

    Daniil2 (#6)
    * adding to settings several statifiles directories, create 1 static directory
    
    * creating page for clubs, adding clubs to the main page
    
    * move static dir to the other place
    
    * created model for clubs, add css for page of clubs, some backend in urls,views
    
    * added pages for resetting password, also added some settings in settings.py for resetting password
    
    * added links in urls.py in authorization, added style for form reset
    
    * added model clubs in the settings
    
    * rewriting the database
    
    * fixed try/except

--- InnoClubs/InnoClubs/apps/authorization/urls.py
+++ InnoClubs/InnoClubs/apps/authorization/urls.py
@@ -1,10 +1,25 @@
 from django.urls import path, include
 from .views import SignUpView
 from . import views
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.login, name='login'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('logout/', views.logout, name='logout'),
     path('main/', include('clubs.urls')),
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(template_name="authorization/password_reset.html"),
+         name="reset_password"),
+
+    path('reset_password_sent/',
+         auth_views.PasswordResetDoneView.as_view(template_name="authorization/password_reset_sent.html"),
+         name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name="authorization/password_reset_form.html"),
+         name="password_reset_confirm"),
+
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name="authorization/password_reset_done.html"),
+         name="password_reset_complete"),
 ]
