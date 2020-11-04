2cedff945536506c00273a26cfe0ac121d0158fe
romanza705 <r.mukhtarov@innopolis.university>
2020-11-04 20:56:26 +0300

    change path of some css files, delete repeated css files, add some HOSTS in settings for seeing design on phone

--- InnoClubs/InnoClubs/settings.py
+++ InnoClubs/InnoClubs/settings.py
@@ -28,7 +28,8 @@ print(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.1.104",
+                 "127.0.0.1"]
 
 
 # Application definition
