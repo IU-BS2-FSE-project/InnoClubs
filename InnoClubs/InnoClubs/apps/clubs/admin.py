4f02f7198bc746bafcc114010498cf02f96dfe17
romanza705 <r.mukhtarov@innopolis.university>
2020-11-05 20:48:19 +0300

    Models of Student, ClubAdmin added. The synchronization between User and Student set.

--- InnoClubs/InnoClubs/apps/clubs/admin.py
+++ InnoClubs/InnoClubs/apps/clubs/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Club
+from .models import Club, Student, ClubAdmin
 
 # Register your models here.
 admin.site.register(Club)
+admin.site.register(Student)
+admin.site.register(ClubAdmin)
+
--- InnoClubs/InnoClubs/apps/clubs/models.py
+++ InnoClubs/InnoClubs/apps/clubs/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-
-# Create your models here.
 
 class Club(models.Model):
     club_title = models.CharField("Title of the club", max_length=200)
@@ -12,3 +13,26 @@ class Club(models.Model):
 
     def __str__(self):
         return self.club_title
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    hours = models.TimeField("Hours", null=True, blank=True)
+    # how to use info from this field - https://metanit.com/python/django/5.7.php
+    subscriptions = models.ManyToManyField(Club, null=True, blank=True)
+
+
+class ClubAdmin(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    club = models.ForeignKey(Club, on_delete=models.CASCADE)
+    rights = models.BinaryField()  # there is only two options(admin and assistant).So let's 1 be admin and 0 assistant
+
+
+# these two methods need for adding default Users records to custom Student
+# link to method https://habr.com/ru/post/313764/#OneToOneField
+# Note: if you want to use data from Student, f.e. hours, you should write user.student.hours
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Student.objects.create(user=instance)
+    instance.student.save()
