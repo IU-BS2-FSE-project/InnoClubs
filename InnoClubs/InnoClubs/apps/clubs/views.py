e9425813e35adbd4a0f66f148caf870c7d5bf811
romanza705 <r.mukhtarov@innopolis.university>
2020-11-08 13:25:28 +0300

    Administration levels added

--- InnoClubs/InnoClubs/apps/clubs/views.py
+++ InnoClubs/InnoClubs/apps/clubs/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import auth
 from django.core.exceptions import ObjectDoesNotExist
 from django.urls import reverse
-from .models import Club
+from .models import Club, Student, ClubAdmin
 from django.http import HttpResponseRedirect
 
 
@@ -49,3 +49,21 @@ def unsubscribe(request, club_url):
     club = Club.objects.get(club_url=club_url)
     user.student.subscriptions.remove(club)
     return HttpResponseRedirect(reverse('clubPage', args=(club_url,)))
+
+
+def administration(request, club_url):
+    args = {}
+    club = Club.objects.get(club_url=club_url)
+    args['user'] = auth.get_user(request)
+    args['club'] = club
+    args['students'] = Student.objects.filter(subscriptions__club_url=club_url)
+    admins_list = []
+    for record in club.clubadmin_set.all():
+        admins_list.append(record.student)
+    args['admins_list'] = admins_list
+
+    return render(request, "clubs/administrationOfClub.html", args)
+
+
+def addEvent(request, club_url):
+    pass
