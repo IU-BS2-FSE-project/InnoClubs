76c4074cf684dfb0ff8494913d7db73e017c8aec
Kirill Ivanov <bragov4ik@hotmail.com>
2020-11-08 15:22:31 +0300

    Shorten lines to comply with linter

--- InnoClubs/InnoClubs/settings.py
+++ InnoClubs/InnoClubs/settings.py
@@ -93,16 +93,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': ('django.contrib.auth.password_validation.'
+                 'UserAttributeSimilarityValidator'),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': ('django.contrib.auth.password_validation.'
+                 'MinimumLengthValidator'),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': ('django.contrib.auth.password_validation.'
+                 'CommonPasswordValidator'),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': ('django.contrib.auth.password_validation.'
+                 'NumericPasswordValidator'),
     },
 ]
 
@@ -138,6 +142,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
-# some gmail account with https://myaccount.google.com/lesssecureapps is turned on
+# some gmail account with
+# https://myaccount.google.com/lesssecureapps is turned on
 EMAIL_HOST_USER = '*********'
 EMAIL_HOST_PASSWORD = '*******'  # password for this account
