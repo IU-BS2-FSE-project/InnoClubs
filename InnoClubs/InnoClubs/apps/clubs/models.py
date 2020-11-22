9cc889919e3aa3e6d81d16d65bd000607a6c1c15
Kirill Ivanov <bragov4ik@hotmail.com>
2020-11-22 23:45:22 +0300

    fix club model

--- InnoClubs/InnoClubs/apps/clubs/models.py
+++ InnoClubs/InnoClubs/apps/clubs/models.py
@@ -18,11 +18,11 @@ class ClubType(models.Model):
 
 class Club(models.Model):
     club_title = models.CharField("Title of the club",
-                                  max_length=200, editable=True)
+                                  max_length=200)
     club_info = models.CharField("Information of the club", max_length=2000)
     club_logo = models.ImageField(upload_to="static/img/", null=True)
     club_url = models.CharField(
-        "Url of the club(For example testUrl)", max_length=200, editable=False)
+        "Url of the club(For example testUrl)", max_length=200)
     club_chat = models.CharField("Telegram chat", max_length=200, null=True)
     club_type = models.ForeignKey(ClubType, on_delete=models.CASCADE,
                                   null=True)
