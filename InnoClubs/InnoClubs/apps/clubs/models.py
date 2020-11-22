c8d705a814374b9152d7656522540dcd6f7edce8
Kirill Ivanov <bragov4ik@hotmail.com>
2020-11-22 16:46:09 +0300

    Return previous club model (as absence of image is handled differently)

--- InnoClubs/InnoClubs/apps/clubs/models.py
+++ InnoClubs/InnoClubs/apps/clubs/models.py
@@ -7,10 +7,7 @@ from django.dispatch import receiver
 class Club(models.Model):
     club_title = models.CharField("Title of the club", max_length=200, editable=False)
     club_info = models.CharField("Information of the club", max_length=2000)
-    club_logo = models.ImageField(
-        upload_to="static/img/",
-        null=False,
-        default="static/img/no_image_found.png")
+    club_logo = models.ImageField(upload_to="static/img/", null=True)
     club_url = models.CharField(
         "Url of the club(For example testUrl)", max_length=200, editable=False)
     club_chat = models.CharField("Telegram chat", max_length=200, null=True)
